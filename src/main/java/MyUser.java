c7755df59e29cb34292f447ee2b33732ae13057f
houyajun <houyajunjava@163.com>
2018-07-22 20:18:21 +0800

    百知项目

--- src/main/java/MyUser.java
+++ src/main/java/MyUser.java
@@ -1,7 +1,6 @@
 public class MyUser {
     public static void main(String[] args) {
         System.out.println("行成于思，毁于随！");
-        System.out.println("诚信者，天下之结也！");
-        System.out.println("言不信者，行必果！");
+        System.out.println("有朋至远方来，不亦乐乎！");
     }
 }
